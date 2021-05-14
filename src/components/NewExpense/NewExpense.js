ffd6a25be981473e6a96fc5145759284758f19ee
Verathagnus <>
2021-05-14 15:06:48 +0000

    Initial commit

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from '../UI/Card';
+import '../UI/Card.css';
+
+const NewExpense = () => {
+  return {
+    <div>
+    <form></form>
+    </div>
+  };
+};
+
+export default NewExpense;
